ea52482295498a852a8017cc6394c824f6f99409
ddsurvivor <liangw1996@gmail.com>
2022-01-23 00:03:41 +0800

    attack system and map

--- StartTheShow/Assets/Scripts/Bullet.cs
+++ StartTheShow/Assets/Scripts/Bullet.cs
@@ -11,14 +11,22 @@ public class Bullet : MonoBehaviour
     // 移动速度
     public float moveSpeedMax;
     public float moveSpeed;
+    public float rotateSpeed;
 
 
     // 造成的伤害
     public int damage;
 
-    public GameObject target;
+    private GameObject target;
+    public GameObject fromTower;
+    public GameObject targetTower;
 
     public bool startMove;
+    public bool ableDamage;
+
+    public BulletType BulletType;
+
+    public GameObject Item;
 
 
     // Start is called before the first frame update
@@ -34,24 +42,52 @@ public class Bullet : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        if (startMove && target!=null)
+        if (startMove && target != null)
         {
             Vector2 toTarget = (target.transform.position - transform.position).normalized;
             //Vector2 toTarget = new Vector2(target.transform.position.x, target.transform.position.y).normalized;
             transform.Translate(toTarget * moveSpeed * Time.deltaTime, Space.World);
+            //float angelZ = Vector2.Angle(transform.position, toTarget);
+            //transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, angelZ));
+            transform.up = Vector2.Lerp(transform.up, toTarget, rotateSpeed);
 ;        }
     }
     public void FollowTarget(GameObject _target)
     {
+        startMove = true;
+        ableDamage = false;
+        target = _target;
+        BulletType = BulletType.NEGATIVE;
+    }
+    public void FollowTarget(GameObject _target, GameObject _tower, GameObject _from)
+    {
         target = _target;
         startMove = true;
+        targetTower = _tower;
+        ableDamage = true;
+        fromTower = _from;
+        BulletType = BulletType.PASITIVE;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Monster"))
+        if (collision.gameObject.CompareTag("Monster") && ableDamage)
         {
             collision.GetComponent<MonsterController>().GetDamage(damage);
+            //Destroy(this.gameObject);
+            target = targetTower;
+            Debug.Log("Bullet: Hit monster");
+        }
+        if (collision.gameObject.CompareTag("NegativeTower") && BulletType == BulletType.PASITIVE)
+        {
+            collision.gameObject.GetComponent<TowerController>().ShotToTower(fromTower);
             Destroy(this.gameObject);
+            Debug.Log("Bullet: Hit tower");
+        }
+        if (collision.gameObject.CompareTag("Tower") && BulletType == BulletType.NEGATIVE)
+        {
+            collision.gameObject.GetComponent<TowerController>().bulletCount++;
+            Destroy(this.gameObject);
+            Debug.Log("Bullet: Hit pasitive tower");
         }
     }
     
--- StartTheShow/Assets/Scripts/TowerController.cs
+++ StartTheShow/Assets/Scripts/TowerController.cs
@@ -9,6 +9,7 @@ public enum TowerType
 public class TowerController : MonoBehaviour
 {
     public GameObject bulletPrefab;
+    public GameObject ngBltPrefab;
     public TowerType TowerType;
 
     // 拥有的子弹的数量
@@ -34,15 +35,27 @@ public class TowerController : MonoBehaviour
     {
         
     }
+    private void LateUpdate()
+    {
+        if (TowerType == TowerType.PASITIVE)
+        {
+            UpdateAttackRange();
+        }
+        
+    }
 
     public void ShotToMonster(GameObject _target)
     {
         if (CostBullet())
         {
             GameObject bullet = GameObject.Instantiate(bulletPrefab, this.transform);
-            bullet.GetComponent<Bullet>().FollowTarget(_target);
-            
+            bullet.GetComponent<Bullet>().FollowTarget(_target, connectTower, this.gameObject);
         }
+    }
+    public void ShotToTower(GameObject _tower)
+    {
+        GameObject bullet = GameObject.Instantiate(ngBltPrefab, this.transform);
+        bullet.GetComponent<Bullet>().FollowTarget(_tower);
     }
     public bool CostBullet()
     {
@@ -60,6 +73,12 @@ public class TowerController : MonoBehaviour
     public void TowerMove()
     {
 
+    }
+    public void UpdateAttackRange()
+    {
+        attackDetect.transform.position = (transform.position + connectTower.transform.position) / 2;
+        attackDetect.GetComponent<BoxCollider2D>().size = new Vector2(Mathf.Abs(transform.position.x - connectTower.transform.position.x),
+                                                                      Mathf.Abs(transform.position.y - connectTower.transform.position.y));
     }
     public void ShowRange(int _range)
     {
